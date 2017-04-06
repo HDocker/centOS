27954fed78f2fab6ecece34b1fbe824f9a5f3825
hoysoft <hoysoft@qq.com>
2017-04-06 10:34:23 +0800

    Update entrypoint.sh

--- centos-ssh/entrypoint.sh
+++ centos-ssh/entrypoint.sh
@@ -3,5 +3,5 @@
 set -e
 
 # 添加用户root，密码root，并且将此用户添加到sudoers里  
-RUN echo "root:${SSH_ROOT_PASSWORD}" | chpasswd  
-RUN echo "root   ALL=(ALL)       ALL" >> /etc/sudoers
+echo "root:${SSH_ROOT_PASSWORD}" | chpasswd  
+echo "root   ALL=(ALL)       ALL" >> /etc/sudoers
