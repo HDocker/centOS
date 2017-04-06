50c488ae89e2f7e8ac02dfb7aaff2da1a699e6fa
hoysoft <hoysoft@qq.com>
2017-04-06 11:58:43 +0800

    Update docker-entrypoint.sh

--- centos-ssh/docker-entrypoint.sh
+++ centos-ssh/docker-entrypoint.sh
@@ -3,6 +3,9 @@
 set -e
 
 # 添加用户root，密码root，并且将此用户添加到sudoers里  
+if [ "$SSH_ROOT_PASSWORD" = '' ]; then
+  export SSH_ROOT_PASSWORD="root"
+fi
 echo "root:${SSH_ROOT_PASSWORD}" | chpasswd  
 echo "root   ALL=(ALL)       ALL" >> /etc/sudoers
 
