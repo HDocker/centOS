c7fba5fa5aae8eea04e9240e23315680654ac838
hoysoft <hoysoft@qq.com>
2017-04-06 17:04:29 +0800

    Update docker-entrypoint.sh

--- centos-ssh/docker-entrypoint.sh
+++ centos-ssh/docker-entrypoint.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-echo $SSH_ROOT_PASSWORD
+if [ -z "$SSH_ROOT_PASSWORD" ]; then
+			echo >&2 'error: root password option is not specified '
+			echo >&2 '  You need to specify one of SSH_ROOT_PASSWORD'
+			exit 1
+
+		fi
+echo >&2 "root passwd: ${SSH_ROOT_PASSWORD}"
 # 添加用户root，密码root，并且将此用户添加到sudoers里  
 #if [ "$SSH_ROOT_PASSWORD"='' ]; then
 #  export SSH_ROOT_PASSWORD="root"
